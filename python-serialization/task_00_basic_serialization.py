d87cdf789867dd1d7de93c428cf37cec6a901d43
Rmxpvl <remy.pinville@gmail.com>
2026-05-26 14:58:33 +0200

    Fix repository root layout

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -3,11 +3,12 @@
 
 import json
 
+
 def serialize_and_save_to_file(data, filename):
     with open(filename, "wb", encoding="utf-8") as file:
         json.dump(data, file)
 
 
 def load_and_deserialize(filename):
-    with open(filename, "rb",encoding="utf-8") as file:
+    with open(filename, "rb", encoding="utf-8") as file:
         return json.load(file)
