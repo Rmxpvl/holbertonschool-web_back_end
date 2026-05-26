926640b22ed0ce752e6f56afc5a64a7ec6310000
Rmxpvl <remy.pinville@gmail.com>
2026-05-26 14:55:46 +0200

    Add higher level programming project

---
+++ holbertonschool-higher_level_programming/python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Basic serialization module"""
+
+import json
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, "wb", encoding="utf-8") as file:
+        json.dump(data, file)
+
+
+def load_and_deserialize(filename):
+    with open(filename, "rb",encoding="utf-8") as file:
+        return json.load(file)
