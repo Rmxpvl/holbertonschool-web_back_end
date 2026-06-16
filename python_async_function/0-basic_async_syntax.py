b9cc585fb1744bcedfd4354418553827ec4d0ea3
Rmxpvl <remy.pinville@gmail.com>
2026-06-16 11:56:16 +0200

    Add docstring to 0-basic_async_syntax.py for clarity

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+"""Code qui lance un compteur random puis renvoi le temps"""
 
 import asyncio
 import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
     delay = random.uniform(0, max_delay)
