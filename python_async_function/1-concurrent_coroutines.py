b59cc1d4fcd5562ed386ed548bbb4c6fdfbcc2cb
Rmxpvl <remy.pinville@gmail.com>
2026-06-19 15:35:49 +0200

    Refactor wait_n function in 1-concurrent_coroutines.py for improved clarity and efficiency

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
-"""Asynchronous coroutine that waits for multiple task to finish and collect delay."""
+"""Script that spawn wait_random n times with the specified max_delay"""
 
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
-    """Wait for multiple coroutines to finish and return the sorted delays."""
-    task_list = []
 
-    for _ in range(n):
-        # Start each coroutine immediately so they run concurrently.
-        task = asyncio.create_task(wait_random(max_delay))
-        task_list.append(task)
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ Return the list of all the delays"""
+    delays = []
 
-    # Wait for every task to finish and collect the delays.
-    results = await asyncio.gather(*task_list)
+    tasks = [wait_random(max_delay) for _ in range(n)]
 
-    # Return the delays sorted from shortest to longest.
-    return sorted(results)
+    for task in asyncio.as_completed(tasks):
+        delayed = await task
+        delays.append(delayed)
+    return delays
