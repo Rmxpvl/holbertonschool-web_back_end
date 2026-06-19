e67e9c853c8496f6c7d334c88e8be5750363d55d
Rmxpvl <remy.pinville@gmail.com>
2026-06-19 16:17:55 +0200

    Refactor measure_time function in 2-measure_runtime.py for clarity and accuracy

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 """Measure the total runtime of wait_n."""
 
-import asyncio
 import time
+import asyncio
+
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
-def measure_time(n: int, max_delay: int) -> List[float]:
-    """Function that calculates the average"""
-    async start = time.time()
+def measure_time(n: int, max_delay: int) -> float:
+    """Return average execution time of wait_n."""
+    start = time.time()
+
     asyncio.run(wait_n(n, max_delay))
-    total_time = time.time() - start_time
+
+    total_time = time.time() - start
+
     return total_time / n
