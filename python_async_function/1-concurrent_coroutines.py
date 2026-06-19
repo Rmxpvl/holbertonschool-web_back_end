6dd9c801249638e4445cc985450663807bf89571
Rmxpvl <remy.pinville@gmail.com>
2026-06-19 15:34:53 +0200

    Refactor import statement in 1-concurrent_coroutines.py for clarity and consistency

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,7 +2,8 @@
 """Asynchronous coroutine that waits for multiple task to finish and collect delay."""
 
 import asyncio
-from wait_random import wait_random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
     """Wait for multiple coroutines to finish and return the sorted delays."""
